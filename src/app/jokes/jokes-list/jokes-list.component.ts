0763d2dedebdcdc95c4abe160e0dd2c976daed9a
JohanurRahman <johanur.rahman@outlook.com>
2020-10-22 04:49:10 +0600

    Pagination Added

--- src/app/jokes/jokes-list/jokes-list.component.ts
+++ src/app/jokes/jokes-list/jokes-list.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Joke } from '../i-jokes';
+import {PageChangedEvent} from 'ngx-bootstrap';
 
 @Component({
   selector: 'app-jokes-list',
@@ -12,25 +13,33 @@ export class JokesListComponent  {
   displayedColumns: string[] = ['joke', 'flags', 'actions'];
   dataSource: Joke[];
 
+  itemsPerPage = 5;
+  jokesList: Joke[];
+
   constructor() {
     this.getJokesFromStorage();
   }
 
   getJokesFromStorage() {
-    this.dataSource = JSON.parse(localStorage.getItem('jokes')) ? JSON.parse(localStorage.getItem('jokes')) : [];
+    this.jokesList = JSON.parse(localStorage.getItem('jokes')) ? JSON.parse(localStorage.getItem('jokes').slice()) : [];
+    this.dataSource = this.jokesList.slice(0, 5);
   }
 
-  playRandomJokes() {
-    for (const item of this.dataSource) {
-      if (item.joke.punchline) {
-        const content = new SpeechSynthesisUtterance(item.joke.content);
-        const punchline = new SpeechSynthesisUtterance(item.joke.punchline);
-        (window as any).speechSynthesis.speak(content);
-        (window as any).speechSynthesis.speak(punchline);
-      } else {
-        const content = new SpeechSynthesisUtterance(item.joke.content);
-        (window as any).speechSynthesis.speak(content);
-      }
-    }
+  // playRandomJokes() {
+  //   for (const item of this.dataSource) {
+  //     if (item.joke.punchline) {
+  //       const content = new SpeechSynthesisUtterance(item.joke.content);
+  //       const punchline = new SpeechSynthesisUtterance(item.joke.punchline);
+  //       (window as any).speechSynthesis.speak(content);
+  //       (window as any).speechSynthesis.speak(punchline);
+  //     } else {
+  //       const content = new SpeechSynthesisUtterance(item.joke.content);
+  //       (window as any).speechSynthesis.speak(content);
+  //     }
+  //   }
+  // }
+
+  pageChanged($event: PageChangedEvent) {
+    this.dataSource = this.jokesList.slice($event.page * this.itemsPerPage - this.itemsPerPage, $event.page * this.itemsPerPage);
   }
 }
--- src/app/jokes/jokes-list/jokes-list.module.ts
+++ src/app/jokes/jokes-list/jokes-list.module.ts
@@ -4,17 +4,19 @@ import { JokesListComponent } from './jokes-list.component';
 import { JokesListRoutingModule } from './jokes-list-routing.module';
 import { MatTableModule } from '@angular/material/table';
 import { MatButtonModule } from '@angular/material/button';
+import {PaginationModule} from 'ngx-bootstrap';
 
 @NgModule({
   declarations: [
     JokesListComponent
   ],
-  imports: [
-    CommonModule,
-    JokesListRoutingModule,
-    MatTableModule,
-    MatButtonModule
-  ]
+    imports: [
+        CommonModule,
+        JokesListRoutingModule,
+        MatTableModule,
+        MatButtonModule,
+        PaginationModule.forRoot()
+    ]
 })
 
 export class JokesListModule { }
