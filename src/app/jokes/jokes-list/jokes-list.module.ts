57ce68aeafd444bf21abaafb26683aee16db9f5f
JohanurRahman <johanur.rahman@outlook.com>
2020-10-22 07:00:45 +0600

    Limit String Pipe Added

--- src/app/jokes/jokes-list/jokes-list.module.ts
+++ src/app/jokes/jokes-list/jokes-list.module.ts
@@ -5,18 +5,20 @@ import { JokesListRoutingModule } from './jokes-list-routing.module';
 import { MatTableModule } from '@angular/material/table';
 import { MatButtonModule } from '@angular/material/button';
 import {PaginationModule} from 'ngx-bootstrap';
+import {LimitStringModule} from '../../pipes/limit-string/limit-string.module';
 
 @NgModule({
   declarations: [
     JokesListComponent
   ],
-    imports: [
-        CommonModule,
-        JokesListRoutingModule,
-        MatTableModule,
-        MatButtonModule,
-        PaginationModule.forRoot()
-    ]
+  imports: [
+    CommonModule,
+    JokesListRoutingModule,
+    MatTableModule,
+    MatButtonModule,
+    PaginationModule.forRoot(),
+    LimitStringModule
+  ]
 })
 
 export class JokesListModule { }
