edb9bce040e271772105f982523676e572e59b76
JohanurRahman <johanur.rahman@outlook.com>
2020-10-22 09:22:21 +0600

    Joke Add/Edit Form Css Updated

--- src/app/jokes/jokes-add/jokes-add.component.ts
+++ src/app/jokes/jokes-add/jokes-add.component.ts
@@ -6,8 +6,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./jokes-add.component.scss']
 })
 
-export class JokesAddComponent  {
-
-  title = 'Add Joke';
-
-}
+export class JokesAddComponent { }
--- src/app/jokes/jokes-edit/jokes-edit.component.ts
+++ src/app/jokes/jokes-edit/jokes-edit.component.ts
@@ -1,22 +1,20 @@
-import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
-import {Joke} from '../i-jokes';
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Joke } from '../i-jokes';
 
 @Component({
   selector: 'app-jokes-edit',
   templateUrl: './jokes-edit.component.html',
   styleUrls: ['./jokes-edit.component.scss']
 })
-export class JokesEditComponent implements OnInit {
+
+export class JokesEditComponent {
 
   title = 'Edit Joke';
-  details: Joke;
+  jokeDetails: Joke;
 
   constructor(private activatedRoute: ActivatedRoute) {
-    activatedRoute.data.subscribe(({ joke }) => this.details = joke);
-  }
-
-  ngOnInit() {
+    activatedRoute.data.subscribe(({ joke }) => this.jokeDetails = joke);
   }
 
 }
