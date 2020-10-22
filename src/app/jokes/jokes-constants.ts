faf9ecf2a63820027aae09a03341fb142d2b328e
JohanurRahman <johanur.rahman@outlook.com>
2020-10-22 09:46:10 +0600

    Demo Jokes Added

--- src/app/jokes/jokes-constants.ts
+++ src/app/jokes/jokes-constants.ts
@@ -12,8 +12,8 @@ const Flags = [
 const Jokes: Joke[] = [
   {
     id: 1,
-    content: 'I went to buy some camo pants but couldn’t find any',
-    category: 'Programming',
+    content: ' I failed math so many times at school, I can’t even count.',
+    category: 'Pun',
     type: 'Single',
     flags: [
       {title: 'NSFW', selected: true},
@@ -24,7 +24,7 @@ const Jokes: Joke[] = [
   },
   {
     id: 2,
-    content: 'My wife told me to stop impersonating a flamingo. I had to put my foot down',
+    content: 'I was wondering why the frisbee kept getting bigger and bigger, but then it hit me',
     category: 'Pun',
     type: 'Single',
     flags: [
@@ -36,9 +36,22 @@ const Jokes: Joke[] = [
   },
   {
     id: 3,
+    content: 'Why did David Hasselhoff change his name to “The Hoff?”',
+    category: 'Miscellaneous',
+    punchline: 'It’s less hassle',
+    type: 'Two Part',
+    flags: [
+      {title: 'NSFW', selected: false},
+      {title: 'Religious', selected: true},
+      {title: 'Political', selected: false},
+      {title: 'Racist', selected: false}
+    ]
+  },
+  {
+    id: 4,
     content: 'Why does Waldo wear stripes?',
     category: 'Miscellaneous',
-    punchline: 'Because he doesnt want to be spotted',
+    punchline: 'Because he doesn’t want to be spotted',
     type: 'Two Part',
     flags: [
       {title: 'NSFW', selected: false},
